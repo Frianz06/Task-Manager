06c12d0d31aaaf9e651d83b45f85ddfa25ead383
Ykoo_ <Ychickolegaspi@gmail.com>
2025-10-01 02:39:02 +0800

    nagkaroon na ng login ahahahha basta meron bago
    may login page na pero may error din kapag may tinesting, malalaman pa soon ano ba to ahahahah 2:23am na

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,21 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: () => import('@/stores/views/guest/LoginView.vue'),
+      
+    },
+
+    {
+      path: '/',
+      name: 'TaskManager',
+      component: () => import('@/stores/views/auth/TaskManager.vue'),
+      
+    }
+  ],
 })
 
 export default router
